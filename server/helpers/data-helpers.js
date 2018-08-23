de1ca1c518f4e228fdbaee1e88cc5440892b04ab
Rex Chiu <rex.chiu@outlook.com>
2018-08-23 15:25:40 -0400

    added API serializer

--- server/helpers/data-helpers.js
+++ server/helpers/data-helpers.js
@@ -1,7 +1,14 @@
 module.exports = function makeDataHelpers(dbURL) {
   return {
     findAll() {
-      return 'Cats';
+      return [
+        { id: 1, name: 'Cats', type: 'Cats' },
+        { id: 2, name: 'Dogs', type: 'Dogs' },
+        { id: 3, name: 'Alpacas', type: 'Alpacas' }
+      ];
+    },
+    find(id) {
+      return { id: 1, name: 'Cats', type: 'Cats' };
     }
   };
 };
--- server/routes/pets.js
+++ server/routes/pets.js
@@ -1,15 +1,31 @@
 import express from 'express';
 
+const JSONAPISerializer = require('jsonapi-serializer').Serializer;
+
 const router = express.Router();
 
 /* GET index page. */
 module.exports = (dataHelpers) => {
+  const PetsSerializer = new JSONAPISerializer('pets', {
+    attributes: ['name', 'type']
+  });
+  const PetSerializer = new JSONAPISerializer('pet', {
+    attributes: ['name', 'type']
+  });
+
   router.get('/', (req, res) => {
     const result = dataHelpers.findAll();
+    const pets = PetsSerializer.serialize(result);
 
-    res.json({
-      title: result
-    });
+    res.json(pets);
+  });
+
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const result = dataHelpers.find(id);
+    const pet = PetSerializer.serialize(result);
+
+    res.json(pet);
   });
 
   return router;
