9fa2f67b5fad490b4cf5c4cc800a48004e86e4a8
Jaron Evans <jarondevans@gmail.com>
2018-08-24 16:42:04 -0400

    login form

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 //import assets
 
 class Home extends Component {
-
-
-
   render() {
     return (
-      <div>
-        <h2>Home</h2>
-      </div>
+      <form method="post" id="login" action="/user/login">
+        <label>
+          username:
+          <input name="username" type="text" onChange={this.handleChange} />
+        </label>
+        <label>
+          password:
+          <input name="password" type="password" name="password" />
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
