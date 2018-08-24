3be924dbe7b2e6e936a6cf8f91d61795ca61a5cd
Jaron D Evans <jarondevans@gmail.com>
2018-08-24 00:08:06 +0000

    server adjusted/ printed

--- client/src/App.js
+++ client/src/App.js
@@ -12,19 +12,69 @@ import Navbar from './Navbar.jsx'
 import './App.css';
 
 
-class App extends Component {
+
+
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        pets: []
+      };
+    }
+
+// $.ajax({
+//   url: "test.html",
+//   context: document.body
+//   }).done(function() {
+//   $( this ).addClass( "done" );
+// });
 
   componentDidMount() {
   //code I added in will link to server
+  fetch("http://localhost:8080/pets")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          pets: result.data
+        });
+      },
+
+      (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
+
+
+
+
 
 
 //       //              RENDERING
   render() {
+    const { pets } = this.state
     return (
+
+
+
       <div className="App">
         <Navbar />
+        <ul>
+          {pets && pets.map((pet, i) => <li key={i}>{pet.type}</li>)}
+        </ul>
       </div>
+
+
+
+
     );
   }
 }
