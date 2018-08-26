3dc9babbe4a4a223ff1abb4320e8d905dbb46b55
Rex Chiu <rex.chiu@outlook.com>
2018-08-26 12:28:37 -0400

    added comments to /register

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,19 +9,22 @@ const SALT_ROUNDS = 10;
 /* GET index page. */
 module.exports = (dataHelpers) => {
   router.post('/register', async (req, res) => {
+    // generates a password hash
     const passwordDigest = await bcrypt.hash(req.body.password, SALT_ROUNDS);
 
+    // constructs an user object to send to db
     const inputObj = {
       username: req.body.username,
       passwordDigest
     };
 
+    // grabs userId from successful db insert
     const userId = await dataHelpers.insertNewUser(inputObj);
 
+    // constructs return obj and serializes it
     const returnObj = {
       userId
     };
-
     const jsonOutput = UserSerializer.serialize(returnObj);
     res.json(jsonOutput);
   });
