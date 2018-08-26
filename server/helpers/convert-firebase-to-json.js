84d774d9f1fcc9fc10d57485647cb804606f6171
Rex Chiu <rex.chiu@outlook.com>
2018-08-26 12:29:36 -0400

    added more comments

--- server/helpers/convert-firebase-to-json.js
+++ server/helpers/convert-firebase-to-json.js
@@ -1,4 +1,5 @@
 module.exports = function convertToJSONFormat(json) {
+  // parses the firebase output of key + value to only values
   const convertedJson = Object.values(json.pets);
 
   return convertedJson;
