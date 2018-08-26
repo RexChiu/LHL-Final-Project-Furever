8ff6f03ce102feb1ff3016074ff1621745917d3b
Sai Gautam <sai.gautam@gmail.com>
2018-08-26 22:32:07 +0000

    Added the functionality to adopt the pet on the database side

--- server/helpers/data-helpers.js
+++ server/helpers/data-helpers.js
@@ -6,22 +6,10 @@ module.exports = function makeDataHelpers(db) {
   const ref = db.ref('restricted_access/secret_document');
 
   return {
-    findAll() {
-      return [
-        { id: 1, name: 'Cats', animal: 'Cats' },
-        { id: 2, name: 'Dogs', animal: 'Dogs' },
-        { id: 3, name: 'Alpacas', animal: 'Alpacas' }
-      ];
-    },
-    find(id) {
-      return { id, name: 'Cats', animal: 'Cats' };
-    },
-    returnPetDescription() {
-      return ref.once('value').then(snap => snap.val());
-    },
     returnAll() {
       return ref.once('value').then(snap => jsonConverter(snap.val()));
     },
+    // Insert Pet records from the sanitized API to the database.
     insertMultiplePets(jsonInput) {
       const petsRef = ref.child('pets');
 
@@ -59,6 +47,32 @@ module.exports = function makeDataHelpers(db) {
       console.log(newUserRef.key);
       return newUserRef.set(newUser).then(() => newUserRef.key);
     },
+    moveFbRecord(oldRef, newRef) {
+      oldRef.once('value', (snap) => {
+        newRef.set(snap.val(), (error) => {
+          if (!error) {
+            oldRef.remove();
+          } else if (typeof console !== 'undefined' && console.error) {
+            console.error(error);
+          }
+        });
+      });
+    },
+    // This datahelper will check if the provided petID exists(returns boolean) in the database
+    checkPetIDExists(petID) {
+      const usersRef = ref.child('pets');
+      console.log('Within checkPetExists');
+      return new Promise((resolve, reject) => {
+        usersRef
+          .orderByChild('id')
+          .equalTo(petID)
+          .once('value', (snapshot) => {
+            const exists = snapshot.val() !== null;
+            console.log('Value of checkPetIDExists exists ', exists);
+            resolve(exists);
+          });
+      });
+    },
     // This datahelper will check if the provided username exists(returns boolean) in the database
     checkUserExists(userName) {
       const usersRef = ref.child('users');
@@ -69,10 +83,34 @@ module.exports = function makeDataHelpers(db) {
           .equalTo(userName)
           .once('value', (snapshot) => {
             const exists = snapshot.val() !== null;
-            console.log('Value of exists ', exists);
+            console.log('Value of checkUserExists exists ', exists);
             resolve(exists);
           });
       });
+    },
+    // This datahelper will check if the provided userID exists(returns boolean) in the database
+    checkUserIDExists(userID) {
+      const usersRef = ref.child('users');
+      console.log('Within checkUserIDExists');
+      return new Promise((resolve, reject) => {
+        usersRef.child(userID).once('value', (snapshot) => {
+          const exists = snapshot.val() !== null;
+          console.log('Value of checkUserIDExists exists ', exists);
+          resolve(exists);
+        });
+      });
+    },
+    // This datahelper will attach the Pet to a userID into the database
+    async adoptPet(userID, petID) {
+      const usersRef = ref.child(`users/${userID}/adopted`);
+      const petsRef = ref.child(`pets/${petID}`);
+
+      if ((await this.checkPetIDExists(petID)) && (await this.checkUserIDExists(userID))) {
+        console.log('After PetID and UserID exists');
+        this.moveFbRecord(petsRef, usersRef);
+        return true;
+      }
+      return false;
     },
     // This datahelper will return all details for an existing username from the database
     async getUserDetails(userName) {
@@ -89,6 +127,30 @@ module.exports = function makeDataHelpers(db) {
       return null;
     },
     // This datahelper will insert/register a demo record into the database
+    insertNewUser(newUser) {
+      const usersRef = ref.child('users');
+      const newUserRef = usersRef.push();
+      console.log();
+      newUser.id = newUserRef.key;
+      return newUserRef.set(newUser).then(() => newUserRef.key);
+    },
+    // Test Function
+    returnPetDescription() {
+      return ref.once('value').then(snap => snap.val());
+    },
+    // Test function
+    findAll() {
+      return [
+        { id: 1, name: 'Cats', animal: 'Cats' },
+        { id: 2, name: 'Dogs', animal: 'Dogs' },
+        { id: 3, name: 'Alpacas', animal: 'Alpacas' }
+      ];
+    },
+    // Test function
+    find(id) {
+      return { id, name: 'Cats', animal: 'Cats' };
+    },
+    // Test function - inserts 1 new user record to database
     insertNewUserDemo() {
       const usersRef = ref.child('users');
       const newUserRef = usersRef.push();
@@ -98,13 +160,7 @@ module.exports = function makeDataHelpers(db) {
         password: '123'
       });
     },
-    insertNewUser(newUser) {
-      const usersRef = ref.child('users');
-      const newUserRef = usersRef.push();
-      console.log();
-      newUser.id = newUserRef.key;
-      return newUserRef.set(newUser).then(() => newUserRef.key);
-    },
+    // Test function - inserts 1 pet record into database
     insertDemoRecord() {
       const petsRef = ref.child('pets');
       petsRef.push().set({
