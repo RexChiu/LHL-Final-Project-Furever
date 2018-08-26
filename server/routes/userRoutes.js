ad3a31e1898e4384fbaad5a10c1ad83fbbaa26ec
Rex Chiu <rex.chiu@outlook.com>
2018-08-26 11:57:24 -0400

    added server side code to accept registration

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,19 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
+const SALT_ROUNDS = 10;
 
 /* GET index page. */
 module.exports = (dataHelpers) => {
-  router.post('/register', (req, res) => {
+  router.post('/register', async (req, res) => {
+    const passwordDigest = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+
     const inputObj = {
       username: req.body.username,
-      password: req.body.password
+      passwordDigest
     };
+
     res.json(inputObj);
   });
 
