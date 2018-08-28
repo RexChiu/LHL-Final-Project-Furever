63d043edf319fa3aaa5096f7db7d00fd889ed387
Rex Chiu <rex.chiu@outlook.com>
2018-08-28 18:45:59 -0400

    added comments

--- server/helpers/data-helpers.js
+++ server/helpers/data-helpers.js
@@ -5,25 +5,33 @@ import petFilterHelper from './pet-filter-helper';
 
 module.exports = function makeDataHelpers(db) {
   return {
+    // returns all pets from the firestore database
     returnAll() {
       const petsRef = db.collection('pets');
+      // inits empty array
       const resultArr = [];
+      // grabs all the pets under the collection pets
       return petsRef
         .get()
         .then((snapshot) => {
-          // synchronized for loop
+          // loops through snapshot (multiple docs) and pushes into array
           snapshot.forEach(doc => resultArr.push(doc.data()));
           return resultArr;
         })
         .catch(err => err);
     },
+    // inserts multiple pets into firestore db
     insertMultiplePets(pets) {
+      // creates a batch to insert as a group
       const batch = db.batch();
       const petsRef = db.collection('pets');
 
+      // synchronized for loop to specify the document path and inserting
       for (let i = 0; i < pets.length; i++) {
+        // need to toString() the id
         batch.set(petsRef.doc(pets[i].id.toString()), pets[i]);
       }
+      // commits the batch and returns
       return batch
         .commit()
         .then(result => result)
