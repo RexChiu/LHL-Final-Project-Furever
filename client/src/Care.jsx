e55720272de98abac8778785555f8b9e06a4bd5e
Rex Chiu <rex.chiu@outlook.com>
2018-08-29 17:49:22 -0400

    added care page

---
+++ client/src/Care.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+//import assets
+
+class Care extends Component {
+  render() {
+    return <Fragment>Cats.</Fragment>;
+  }
+}
+
+export default Care;
--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Adopt from './Adopt.jsx';
-import Events from './Events';
+import Events from './Events.jsx';
 import Home from './Home.jsx';
+import Care from './Care.jsx';
 
 class Main extends React.Component {
   //       //              RENDERING
@@ -15,6 +16,7 @@ class Main extends React.Component {
         <Route exact path="/" render={props => <Home {...props} setUserId={this.props.setUserId} />} />
         <Route path="/events" render={props => <Events {...props} />} />
         <Route path="/adopt" render={props => <Adopt {...props} />} />
+        <Route path="/care" render={props => <Care {...props} />} />
       </Switch>
     );
   }
