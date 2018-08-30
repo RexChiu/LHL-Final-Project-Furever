c6d86b11a7e05db3275377862fd6b2534de6545c
Jaron D Evans <jarondevans@gmail.com>
2018-08-30 16:56:35 +0000

    updated eventLocals

--- client/src/EventLocals.jsx
+++ client/src/EventLocals.jsx
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 //import assets
 
 class EventLocals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      users: []
+    };
+  }
+
   componentDidMount() {
     //code I added in will link to server
     fetch('http://localhost:8080/user/withpets')
@@ -25,13 +34,25 @@ class EventLocals extends Component {
   }
 
   render() {
-    // const { users } = this.state;
+    const { users } = this.state;
+    let userItems;
+    if (users instanceof Array) {
+      userItems = users.map((user, i) => (
+        <div>
+          <p> user id: </p>
+          <p> {user.id} </p>
+          <p> username: </p>
+          <p> {user.attributes.username} </p>
+          {/* <img src= {user.attributes.pets[0].photos[0]} > */}
+        </div>
+      ));
+    }
+    // console.log(users[0].attributes.id);
     // let userItems = '';
-    return 'Cats';
-
-    // if (users instanceof Array) {
-    //   userItems = users.map((user, i) => <p> user.id </p>);
-    // });
+    return (
+      <p> {userItems} </p>
+      // {users[0].attributes.id};
+    );
   }
 }
 
