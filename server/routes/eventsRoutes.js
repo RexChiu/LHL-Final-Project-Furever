f4322dde3ce228746a5a033b166d5d8b20927222
Jaron D Evans <jarondevans@gmail.com>
2018-09-01 17:17:25 +0000

    events css and adding likes

--- server/routes/eventsRoutes.js
+++ server/routes/eventsRoutes.js
@@ -29,10 +29,28 @@ module.exports = (dataHelpers) => {
     res.json(jsonOutput);
   });
 
+  router.post('/going', async (req, res) => {
+    // guard statment for existing username
+
+    // constructs an user object to send to db
+    const inputObj = {
+      user: req.body.username,
+      userId: req.body.userId,
+      eventId: req.body.eventId
+    };
+
+    // grabs userId from successful db insert
+    const id = await dataHelpers.insertNewMessageGoing(inputObj);
+
+    const returnObj = inputObj;
+    const jsonOutput = EventsSerializer.serialize(returnObj);
+    res.json(jsonOutput);
+  });
+
   router.get('/', async (req, res) => {
     const result = await dataHelpers.returnAllEvents();
 
-    console.log(result);
+    // console.log(result);
 
     const jsonOutput = EventsSerializer.serialize(result);
     // const jsonOutput = result;
