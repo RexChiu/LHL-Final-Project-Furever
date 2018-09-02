01fd9da15b9368e71716f5913e860d89d28b4c85
Rex Chiu <rex.chiu@outlook.com>
2018-09-02 17:38:35 -0400

    added more styling to the nav bar

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -42,7 +42,7 @@ class Home extends Component {
       <Fragment>
         <div class="row">
           <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-login">
+            <div class="panel-login">
               <div class="panel-heading">
                 <div class="row">
                   <div class="col-xs-6">
