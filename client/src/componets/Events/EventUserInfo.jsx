30b5679377518b47b2841ee7368ced0254658404
Rex Chiu <rex.chiu@outlook.com>
2018-09-05 09:58:48 -0400

    fixed text for events user info

--- client/src/componets/Events/EventUserInfo.jsx
+++ client/src/componets/Events/EventUserInfo.jsx
@@ -46,9 +46,9 @@ class EventUserInfo extends Component {
             userItems.push(
               <div className="eventpetinfocolor">
                 <img src={petphoto} alt="notWorking" width="100em" />
-                <p> name: {users[i].attributes.pets[j].name} </p>
-                <p> breed: {users[i].attributes.pets[j].breed} </p>
-                <p> sex: {users[i].attributes.pets[j].sex} </p>
+                <p> Name: {users[i].attributes.pets[j].name} </p>
+                <p> Breed: {users[i].attributes.pets[j].breed} </p>
+                <p> Sex: {users[i].attributes.pets[j].sex} </p>
               </div>
             );
           }
@@ -61,10 +61,10 @@ class EventUserInfo extends Component {
         <div id="eventuserinfo" className="pull-right col-sm-3 col-xs-3 ">
           <h4>
             {' '}
-            <u> user info </u>{' '}
+            <u> My Info </u>{' '}
           </h4>
           <div id="eventsuserinformation">
-            <p> username: {sessionStorage.getItem('username')} </p>
+            <p> Username: {sessionStorage.getItem('username')} </p>
             {/* <p>
               user id: `$
               {sessionStorage.getItem('userId')}`{' '}
@@ -72,7 +72,7 @@ class EventUserInfo extends Component {
           </div>
           <h4>
             {' '}
-            <u> pet info </u>{' '}
+            <u> My Pets </u>{' '}
           </h4>
           <div>{userItems}</div>
         </div>
